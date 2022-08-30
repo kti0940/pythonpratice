5f3d81bc6253c00386284aa7a928ef8e7843b19e
taein Kim <kti0940@gmail.com>
2022-08-30 09:56:52 +0900

    숫자처리함수

---
+++ number_processing_function.py
@@ -0,0 +1,12 @@
+print(abs(-5)) # 절대값 5
+print(pow(4, 2)) # 4^2 = 4 * 4 = 16
+print(max(5, 12)) # 최대값 12
+print(min(5, 12))  # 최소값 5
+print(round(3.14)) # 3
+print(round(4.99)) # 5
+
+from math import *
+
+print(floor(4.99)) # 내림
+print(ceil(3.14)) # 올림
+print(sqrt(16)) # 제곱근
