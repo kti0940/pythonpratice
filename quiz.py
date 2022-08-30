2ad3756392d2bd58aee2fcffd6bb4aa81f203f22
taein Kim <kti0940@gmail.com>
2022-08-30 15:50:11 +0900

    Quiz 6

--- quiz.py
+++ quiz.py
@@ -26,15 +26,26 @@
 # print("치킨 당첨자 : {0}".format(winners[0]))
 # print("치킨 당첨자 : {0}".format(winners[1:]))
 
-from random import *
+# from random import *
 
-count = 0
-for i in range(1, 51):
-    time = randrange(5, 51)
-    if 5 <= time <= 15:
-        print("[O] {0}번째 손님 (소요시간 : {1}분)".format(i, time))
-        count += 1
-    else: # 매칭 실패한 경우
-        print("[] {0}번째 손님 (소요시간 : {1}분)".format(i, time))
+# count = 0
+# for i in range(1, 51):
+#     time = randrange(5, 51)
+#     if 5 <= time <= 15:
+#         print("[O] {0}번째 손님 (소요시간 : {1}분)".format(i, time))
+#         count += 1
+#     else: # 매칭 실패한 경우
+#         print("[] {0}번째 손님 (소요시간 : {1}분)".format(i, time))
         
-print("총 탑승 승객 : {0}분".format(count))
+# print("총 탑승 승객 : {0}분".format(count))
+
+def std_weight(height, gender):
+    if gender == "남자":
+        return(height * height * 22)
+    else:
+        return(height * height * 21)
+
+height = 175
+gender = "남자"
+weight = round(std_weight(height / 100, gender),2)
+print(weight)
