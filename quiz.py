1b45bede27d7efbbc159476150549845053bc2db
taein Kim <kti0940@gmail.com>
2022-08-30 14:39:33 +0900

    Quiz 5

--- quiz.py
+++ quiz.py
@@ -14,14 +14,27 @@
 
 # print(url[7:].split(".")[0][:3] + str(len(my_str)) + str(my_str.count("e")) + "!")
 
+# from random import *
+# users = range(1,21) # 1부터 20까지 숫자를 생성
+# users = list(users)
+# print(users)
+# shuffle(users)
+# print(users)
+# winners = sample(users, 4) # 4명중에서 1명은 치킨 3명은 커피
+# print(winners)
+# print(" -- 당첨자 발표 --")
+# print("치킨 당첨자 : {0}".format(winners[0]))
+# print("치킨 당첨자 : {0}".format(winners[1:]))
+
 from random import *
-users = range(1,21) # 1부터 20까지 숫자를 생성
-users = list(users)
-print(users)
-shuffle(users)
-print(users)
-winners = sample(users, 4) # 4명중에서 1명은 치킨 3명은 커피
-print(winners)
-print(" -- 당첨자 발표 --")
-print("치킨 당첨자 : {0}".format(winners[0]))
-print("치킨 당첨자 : {0}".format(winners[1:]))
+
+count = 0
+for i in range(1, 51):
+    time = randrange(5, 51)
+    if 5 <= time <= 15:
+        print("[O] {0}번째 손님 (소요시간 : {1}분)".format(i, time))
+        count += 1
+    else: # 매칭 실패한 경우
+        print("[] {0}번째 손님 (소요시간 : {1}분)".format(i, time))
+        
+print("총 탑승 승객 : {0}분".format(count))
