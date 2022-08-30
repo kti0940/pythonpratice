f02418a0eff41abace1b91cd5cf2af3ce9e31813
taein Kim <kti0940@gmail.com>
2022-08-30 11:12:33 +0900

    탈출문자

--- quiz.py
+++ quiz.py
@@ -1,5 +1,15 @@
-from random import *
+# from random import *
 
-day = randint(4, 28)
+# day = randint(4, 28)
 
-print("오프라인 스터디 모임 날짜는 매월" + str(day) + "일로 선정되었습니다")
+# print("오프라인 스터디 모임 날짜는 매월" + str(day) + "일로 선정되었습니다")
+
+url = "http://naver.com"
+
+my_str = url.replace("http://", "")
+my_str = my_str[:my_str.index(".")]
+password = my_str[:3] + str(len(my_str)) + str(my_str.count("e")) + "!"
+
+print(password)
+
+print(url[7:].split(".")[0][:3] + str(len(my_str)) + str(my_str.count("e")) + "!")
