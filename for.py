9bd52e85dfe1d5999d7d6221806fbce1d5d0fdef
taein Kim <kti0940@gmail.com>
2022-08-30 14:07:53 +0900

    while

--- for.py
+++ for.py
@@ -1,8 +1,6 @@
-# from random import randrange
+for waiting_no in range(1,6):
+    print("대기번호 : {0}".format(waiting_no))
 
-# for waiting_no in range(1,6):
-#     print("대기번호 : {0}".format(waiting_no))
-
-starbucks = ["아이언맨", "토르", "아이엠 그루트"]
-for customer in starbucks:
-    print("{0}, 커피가 준비 되었습니다.".format(customer))
+# starbucks = ["아이언맨", "토르", "아이엠 그루트"]
+# for customer in starbucks:
+#     print("{0}, 커피가 준비 되었습니다.".format(customer))
---
+++ while.py
@@ -0,0 +1,26 @@
+# while
+
+# customer = "토르"
+# index = 5
+
+# while index >= 1:
+#     print("{0}, 커피가 준비 되었습니다. {1}번 남았어요".format(customer, index))
+#     index -= 1
+#     if index == 0:
+#         print("커피는 버렸습니다")
+
+# customer = "아이언맨"
+# index = 1
+# while True:
+#     print("{0}님 커피 준비되었습니다. 호출{1}회".format(customer, index))
+#     index += 1
+
+
+customer = "토르"
+person = "Unknown"
+
+while person != customer :
+    print("{0}님 커피가 준비되었습니다".format(customer))
+    person = input("이름이 어떻게 되세요?")
+    if person == "토르":
+        print("커피 맛있게 드세요")
