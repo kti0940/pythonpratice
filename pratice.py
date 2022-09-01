889406db1bab504720eed154642c6fd4988da6cf
taein Kim <kti0940@gmail.com>
2022-09-01 16:37:51 +0900

    모듈 위치 및 삽입

--- pratice.py
+++ pratice.py
@@ -43,3 +43,8 @@ from travel import *
 # trip_to = vietnam.VietnamPackage()
 trip_to = thailand.ThailandPackage()
 trip_to.detail()
+
+import inspect
+import random
+print(inspect.getfile(random))
+print(inspect.getfile(thailand))
